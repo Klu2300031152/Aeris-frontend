b5977cb727cf4c9bf3667d57098e7ad513e1cc59
Klu2300031152 <162615785+Klu2300031152@users.noreply.github.com>
2025-11-28 00:49:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   
   return (
     <>
-      <Router basename="/Aeris-frontend">
+      <Router basename="/">
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />}>
