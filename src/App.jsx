ae2335571e3a2285bc4db90a7be7a29cb07aacec
Klu2300031152 <162615785+Klu2300031152@users.noreply.github.com>
2025-11-28 08:10:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Login from './components/Login';
+import Signup from './components/Signup';
 import HomePage from './components/HomePage';
-import AboutUs from './components/AboutUs';
-import Home from './components/Home';
+import AboutUs from './components/Aboutus';
 import Computers from './components/Computers';
 import Mobiles from './components/Mobiles';
 import Laptops from './components/Laptops';
 import Pendrives from './components/Pendrives';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Home from './components/Home';
 import ProductPage from './components/ProductPage';
 import Cart from './components/Cart';
 import Payment from './components/Payment';
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <>
-      <Router>
+      <Router basename="/">
         <Routes>
           <Route path="/" element={<HomePage />}>
             <Route index element={<AboutUs />} />
@@ -44,4 +44,3 @@ function App() {
 }
 
 export default App;
-
